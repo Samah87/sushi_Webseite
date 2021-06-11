44c5dad89a41e97ba280f4a571b4194b19c85889
samah maher <lilasbluma@gmail.com>
2021-06-11 15:22:27 +0200

    Add project files.

---
+++ inc/header.php
@@ -0,0 +1,4 @@
+<header>
+  <div class="slide"><img src="img/ID-100451651.jpg" alt=""><img src="img/ID-100543480.jpg" alt=""><img src="img/ID-100560629.jpg" alt=""><img src="img/ID-100560821.jpg" alt=""></div>
+  <div id="logo"><img src="img/logo.png" alt=""></div>
+</header>
